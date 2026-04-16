944fe4248426525cc75961890f72e35948811a79
voidgogo <2021038@daelim.ac.kr>
2026-04-16 11:57:49 +0900

    week07: memory management v1.2

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
-
+// 2d array : stack memory & heap memory
 int main() {
-	int rowsColumns[3][5];
+	int columns = 0;
+	int* arr[3]; // statck memory
+	cin >> columns;
 
 	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 5; j++)
-			rowsColumns[i][j] = i * j;
+		arr[i] = new int[columns];  // heap memory
 
 	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 5; j++) {
-			cout << rowsColumns[i][j] << " ";
+		for (int j = 0; j < columns; j++) {
+			arr[i][j] = i * j;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < columns; j++) {
+			cout << arr[i][j] << " ";
 		}
 		cout << '\n';
 	}
-	cout << '\n';
 
+	for (int i = 0; i < 3; i++) {
+		delete[] arr[i];
+		arr[i] = nullptr;
+	}
 	return 0;
 }
