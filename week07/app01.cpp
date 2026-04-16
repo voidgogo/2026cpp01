0d633c8c7d5c6ae4bca3c223891c025e2223a8c9
voidgogo <2021038@daelim.ac.kr>
2026-04-16 12:15:36 +0900

    week07: memory management v1.3

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 using namespace std;
-// 2d array : stack memory & heap memory
+// 2d array : only heap memory
 int main() {
-	int columns = 0;
-	int* arr[3]; // statck memory
+	int rows =0, columns = 0;
+	int** arr = nullptr; 
+	cin >> rows;
 	cin >> columns;
 
-	for (int i = 0; i < 3; i++)
-		arr[i] = new int[columns];  // heap memory
+	arr = new int* [rows];  // 힙메모리에 포인터 배열을 동적할당
+	for (int i = 0; i < rows; i++)
+		arr[i] = new int[columns];  // 힙메모리에 정수 타입을 담는 배열 동적 할당
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			arr[i][j] = i * j;
 		}
 	}
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			cout << arr[i][j] << " ";
 		}
 		cout << '\n';
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		delete[] arr[i];
 		arr[i] = nullptr;
 	}
+	delete[] arr;
+	arr = nullptr;
 	return 0;
 }
