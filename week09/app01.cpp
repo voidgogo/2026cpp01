b3d16551b1228703c0c854a013a882de5db519a5
voidgogo <2021038@daelim.ac.kr>
2026-04-30 12:20:27 +0900

    week09: string v2.0

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <cstring>  // string.h
-using namespace std;
-
-int myStrlen(const char* s) {
-	int count = 0;
-	while(s[count] != '\0') {  // while(s[i] != 0) {  // while(s[i] != NULL) {
-		count++;
-	}
-	return count;
-}
-
-int main() {
-	char s1[] = "ACE";  // array compact initializer
-	const char* ps1 = "ACE~~~";  // string literal
-	cout << s1 << '\n';
-	cout << ps1 << '\n';
-	//cout << strlen(s1) << '\n';
-	cout << strlen(&s1[0]) << '\n';
-	cout << strlen(ps1) << '\n';
-	cout << myStrlen(&s1[0]) << '\n';
-	cout << myStrlen(s1) << '\n';
-	cout << myStrlen(ps1) << '\n';
-
-	return 0;
-}
+//#include <iostream>
+//#include <cstring>  // string.h
+//using namespace std;
+//
+//int myStrlen(const char* s) {
+//	int count = 0;
+//	while(s[count] != '\0') {  // while(s[count] != 0) {  // while(s[count] != NULL) {
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main() {
+//	char s1[] = "ACE";  // array compact initializer
+//	const char* ps1 = "ACE~~~";  // string literal
+//	cout << s1 << '\n';
+//	cout << ps1 << '\n';
+//	//cout << strlen(s1) << '\n';
+//	cout << strlen(&s1[0]) << '\n';
+//	cout << strlen(ps1) << '\n';
+//	cout << myStrlen(&s1[0]) << '\n';
+//	cout << myStrlen(s1) << '\n';
+//	cout << myStrlen(ps1) << '\n';
+//
+//	return 0;
+//}
