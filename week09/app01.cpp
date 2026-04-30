23ac76b541a1eba28e7306c6319bb502541fc9f2
voidgogo <2021038@daelim.ac.kr>
2026-04-30 11:58:17 +0900

    week09: string v1.9

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -2,11 +2,25 @@
 #include <cstring>  // string.h
 using namespace std;
 
+int myStrlen(const char* s) {
+	int count = 0;
+	while(s[count] != '\0') {  // while(s[i] != 0) {  // while(s[i] != NULL) {
+		count++;
+	}
+	return count;
+}
+
 int main() {
 	char s1[] = "ACE";  // array compact initializer
-	const char* ps1 = "ACE";  // string literal
+	const char* ps1 = "ACE~~~";  // string literal
 	cout << s1 << '\n';
 	cout << ps1 << '\n';
+	//cout << strlen(s1) << '\n';
+	cout << strlen(&s1[0]) << '\n';
+	cout << strlen(ps1) << '\n';
+	cout << myStrlen(&s1[0]) << '\n';
+	cout << myStrlen(s1) << '\n';
+	cout << myStrlen(ps1) << '\n';
 
 	return 0;
 }
