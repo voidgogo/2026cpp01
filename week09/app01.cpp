4d9fbefb6e226316ebd9288e46d2867ccab57179
voidgogo <2021038@daelim.ac.kr>
2026-04-30 11:28:53 +0900

    week09: string v1.7

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -8,14 +8,16 @@ int main() {
 	//char s1[] = { 'A', 'C', 'E', NULL };	
 	//char s1[] = { 'A', 'C', 'E', 0 };
 	char s1[] = "ACE";
-	char* ps1 = s1;
+	//char* ps1 = s1;
+	char* ps1 = &s1[0];
 	cout << s1 << '\n';
 	cout << *ps1 << '\n';
 	cout << ps1 << '\n';
 	cout << *s1 << '\n';
 	cout << s1[1] << '\n';
 	cout << *(ps1+1) << '\n';
-	*(ps1 + 2) = 'Z';
+	//*(ps1 + 2) = 'Z';
+	ps1[2] = 'Z';
 	cout << s1 << '\n';
 
 	return 0;
