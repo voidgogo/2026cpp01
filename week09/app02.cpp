b4cce74059a81a1c4708cb84d83d3ddc2bee0224
voidgogo <2021038@daelim.ac.kr>
2026-04-30 12:33:21 +0900

    week09: string v2.1

--- week09/app02.cpp
+++ week09/app02.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <string>
 #include <cstring>
 using namespace std;
 
 int main()
 {
-    char lines[80];
-    cout << "Input : ";
-    cin.getline(lines, 80);  // abc xyz (엔터키가 구분자)
-    //cin >> lines;  // abc xyz
-    cout << endl;
-    cout << "Output: " << endl;
-    cout << lines << endl; // abc (스페이스바, 엔터키, 탭키는 모두 구분자)
+    char a[] = "ACE";  // ACB, ACZ
+    char b[] = "ACE";
+    string c = "ACE";
+    string d = "ACE";
+
+    cout << strcmp(a, b) << '\n';  // 0, -1, 1
+    cout << (c == d) << '\n';  
 
     return 0;
 }
