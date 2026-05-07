f0496824b755b3754a7e23481784f0b56f211c4c
voidgogo <2021038@daelim.ac.kr>
2026-05-07 12:29:28 +0900

    week10: string v2.8

--- week10/app02.cpp
+++ week10/app02.cpp
@@ -15,6 +15,11 @@ int main()
 	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
 	s1 = "12345678901234567";
 	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
+	string s2(s1); // copy constructor
+	cout << s2 << "/" << s2.size() << "/" << s2.capacity() << endl;
+	cout << s2.empty() << '\n';
+	s2.clear();
+	cout << s2.empty() << '\n';
 
 	return 0;
 }
