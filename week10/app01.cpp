95d2a36229eb0b6856bc7886975bfde0ff6c54d0
voidgogo <2021038@daelim.ac.kr>
2026-05-07 10:53:38 +0900

    week10: string v2.3

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -5,17 +5,9 @@ using namespace std;
 
 int main()
 {
-    char a[] = "ACE";  // ACB, ACZ
-    char b[] = "ACE";
-    string c = "ACE";
-    string d = "ACE";
-
-    cout << strcmp(a, b) << '\n';  // 0(두 문자열이 같을때) /  -1, 1(두 문자열이 다를 때)
-    cout << (c == d) << '\n';  // 1(true), 0(false)
-
-    //a = "XYZ";
-    c = "XYZ!";
-    cout << c << '\n';
+    //char* p = "ACE";   // string literal, error!!
+    const char* p = "ACE";  // string literal(constant), read only
+    cout << p << '\n';
 
     return 0;
 }
