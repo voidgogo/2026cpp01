1fe17ba830196a51931dd449ec0737cdc6410030
voidgogo <2021038@daelim.ac.kr>
2026-05-14 11:43:31 +0900

    week11: string v3.0

--- week11/app01.cpp
+++ week11/app01.cpp
@@ -3,22 +3,27 @@
 #include <iomanip>
 using namespace std;
 
+void reverse(string& s)
+{
+	string temp = s;
+	int size = s.size();
+	for (int i = 0; i < size; i++)
+	{
+		s[i] = temp[size - 1 - i];
+	}
+}
+
 int main()
 {
 	string s1("abc");
 	string s2 = "abcd";
-	cout << (s1 == s2) << endl;
-	cout << boolalpha << (s1 == s2) << endl;  // true/false 형태 출력 지정
-	cout << (s1 == s2) << endl;
-	cout << noboolalpha;  // 기본 형태로 지정, 0과 1
-	cout << (s1 == s2) << endl;
 
-	cin >> s2;
-	cout << s2 << endl;
-	//getline(cin, s2, ' ');
-	// 남아있는 더미값을 제거하기 위해 버퍼를 비워야 한다		
-	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	getline(cin, s2);
+	//cin >> s2;
+	cout << s2 << endl;
+	cout << s2[1] << endl;
+	cout << s2.at(1) << endl;
+	reverse(s2);
 	cout << s2 << endl;
 
 	return 0;
